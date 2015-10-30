f3e68c9eb0fa81a5ad3c003f680c6941b49e83e0
revesansparole <revesansparole@gmail.com>
2015-10-30 22:53:01 +0100

    more testing

--- test/test_option/test_example/test_config.py
+++ test/test_option/test_example/test_config.py
@@ -1,6 +1,26 @@
+from nose.tools import assert_raises, with_setup
 import mock
+from os import chdir, mkdir
+from os.path import exists
+from shutil import rmtree
 
 from pkglts.option.example.config import main
+
+
+tmp_dir = "tmp_cfgex"
+
+
+def setup_func():
+    if not exists(tmp_dir):
+        mkdir(tmp_dir)
+
+    chdir(tmp_dir)
+
+
+def teardown_func():
+    chdir("..")
+    if exists(tmp_dir):
+        rmtree(tmp_dir)
 
 
 def test_config_returns_none():
@@ -60,3 +80,17 @@ def test_config_copy_files():
             main(pkg_cfg, dict(option_name='base'))
 
     assert count[0] == 1
+
+
+@with_setup(setup_func, teardown_func)
+def test_config_do_not_overwrite_files():
+    pkg_cfg = dict(base=dict(pkg_fullname='toto',
+                             pkgname='toto',
+                             namespace=None),
+                   hash={})
+
+    # create files
+    main(pkg_cfg, dict(option_name='base'))
+
+    # check that rewriting them raises error
+    assert_raises(UserWarning, lambda: main(pkg_cfg, dict(option_name='base')))
