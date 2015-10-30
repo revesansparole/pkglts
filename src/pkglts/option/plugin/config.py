6f6ccd1f9cc5d85817906010a78bb0a5d0983147
revesansparole <revesansparole@gmail.com>
2015-10-30 19:00:21 +0100

    removed plugin from master

--- src/pkglts/option/plugin/config.py
+++
@@ -1,2 +0,0 @@
-def main(pkg_cfg, extra):
-    return dict(option=None)
--- test/test_option/test_plugin/test_handlers.py
+++
@@ -1,6 +0,0 @@
-from pkglts.option.plugin.handlers import setup_handler
-
-
-def test_handlers():
-    assert True
-    # TODO
