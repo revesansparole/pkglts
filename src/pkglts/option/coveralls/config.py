ad814be2b6f0be3afab973434fc9f8ef33c9e149
revesansparole <revesansparole@gmail.com>
2015-11-12 22:09:25 +0100

    deal with unused arguments

--- src/pkglts/option/coveralls/config.py
+++ src/pkglts/option/coveralls/config.py
@@ -1,3 +1,4 @@
-
 def main(pkg_cfg, extra):
+    del pkg_cfg  # unused
+    del extra  # unused
     return dict(option=None)
--- src/pkglts/option/landscape/config.py
+++ src/pkglts/option/landscape/config.py
@@ -1,3 +1,5 @@
 
 def main(pkg_cfg, extra):
+    del pkg_cfg  # unused
+    del extra  # unused
     return dict(option=None)
