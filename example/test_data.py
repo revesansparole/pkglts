733fb058655103b9fb4cdefbb1b07653e40c465e
revesansparole <revesansparole@gmail.com>
2015-11-24 11:39:18 +0100

    manage namespace and data 'by hand'

--- example/test_data.py
+++ example/test_data.py
@@ -1,4 +1,8 @@
-from oa.toto.data_access import get_data_dir, ls
+try:
+    from toto.data_access import get_data_dir, ls
+except ImportError:
+    print("try namespace")
+    from oa.toto.data_access import get_data_dir, ls
 
 print("data dir", get_data_dir())
 print(ls("."))
