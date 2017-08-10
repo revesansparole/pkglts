da07fb22da7ce8655ecb1044306d88b6478fd3e5
jchopard <jerome.chopard@itk.fr>
2017-08-10 18:02:52 +0200

    added option fmt in doc

--- test/test_option/test_doc/test_config.py
+++ test/test_option/test_doc/test_config.py
@@ -5,29 +5,34 @@ from pkglts.option.doc.config import check, require, update_parameters
 def test_update_parameters():
     cfg = {}
     update_parameters(cfg)
-    assert len(cfg['doc']) == 2
+    assert len(cfg['doc']) == 3
 
 
 def test_config_check_description_exists():
-    cfg = Config(dict(doc={'description': "mydescr", 'keywords': []}))
+    cfg = Config(dict(doc={'description': "mydescr", 'fmt': 'rst', 'keywords': []}))
     assert cfg['doc']['description'] == "mydescr"
     assert 'doc.description' not in check(cfg)
 
 
 def test_config_check_description_valid():
-    cfg = Config(dict(doc={'description': "", 'keywords': []}))
+    cfg = Config(dict(doc={'description': "", 'fmt': 'rst', 'keywords': []}))
     assert 'doc.description' in check(cfg)
 
 
 def test_config_check_keywords_exists():
-    cfg = Config(dict(doc={'description': "mydescr", 'keywords': []}))
+    cfg = Config(dict(doc={'description': "mydescr", 'fmt': 'rst', 'keywords': []}))
     assert len(cfg['doc']['keywords']) == 0
     assert 'doc.keywords' not in check(cfg)
 
 
+def test_config_check_fmt_valid():
+    cfg = Config(dict(doc={'description': "mydescr", 'fmt': 'walou', 'keywords': []}))
+    assert 'fmt' in check(cfg)
+
+
 def test_require():
     cfg = Config(dict(base={}, doc={}))
-
+    
     assert len(require('option', cfg)) == 1
     assert len(require('setup', cfg)) == 0
     assert len(require('install', cfg)) == 0
