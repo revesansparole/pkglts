bdb870656d0f4e5a083dea383b08d9f8a64bb410
jchopard <jerome.chopard@itk.fr>
2017-08-10 18:41:23 +0200

    use block object

--- src/pkglts/templating.py
+++ src/pkglts/templating.py
@@ -8,6 +8,18 @@ block_re = re.compile(r"\{#[ ]pkglts,[ ](?P<key>.*?)\n(?P<cnt>.*?)#\}",
                       re.DOTALL | re.MULTILINE)
 
 
+class TplBlock(object):
+    """Simple container for templated blocks."""
+    
+    def __init__(self):
+        self.before_header = ""
+        self.bid = None
+        self.after_header = ""
+        self.content = ""
+        self.before_footer = ""
+        self.after_footer = ""
+
+
 def parse_source(txt):
     """Parse text to find preserved blocks
 
@@ -23,33 +35,44 @@ def parse_source(txt):
     """
     blocks = []
     last_end = 0
-
+    
     for res in block_re.finditer(txt):
         i = res.start()
         while i > last_end and txt[i] != "\n":
             i -= 1
-
+        
         if i >= last_end:
-            blocks.append((None, "", txt[last_end: (i + 1)], ""))
-
+            b = TplBlock()
+            b.bid = None
+            b.content = txt[last_end: (i + 1)]
+            blocks.append(b)
+        
         bef = txt[(i + 1): res.start()]
-
+        
         bid = res.group('key')
-
+        
         cnt = res.group('cnt')
-
+        
         i = len(cnt) - 1
         while i > 0 and cnt[i] != "\n":
             i -= 1
         aft = cnt[(i + 1): len(cnt)]
         cnt = cnt[:i]
         last_end = res.end() + 1
-
-        blocks.append((bid, bef, cnt, aft))
-
+        
+        b = TplBlock()
+        b.bid = bid
+        b.before_header = bef
+        b.content = cnt
+        b.before_footer = aft
+        blocks.append(b)
+    
     if last_end < len(txt):
-        blocks.append((None, "", txt[last_end: len(txt)], ""))
-
+        b = TplBlock()
+        b.bid = None
+        b.content = txt[last_end: len(txt)]
+        blocks.append(b)
+    
     return blocks
 
 
@@ -69,46 +92,43 @@ def render(cfg, src_pth, tgt_pth):
     # parse src file to find 'preserved' blocks
     with open(src_pth, 'r') as f:
         src_blocks = parse_source(f.read())
-
+    
     blocks = []
     if exists(tgt_pth):  # retrieves preserved blocks from source
         # parse tgt file to find 'preserved' blocks
         with open(tgt_pth, 'r') as f:
             tgt_blocks = parse_source(f.read())
-
-        src_blocks = dict((bid, cnt) for (bid, bef, cnt, aft) in src_blocks
-                          if bid is not None)
-        for bid, bef, cnt, aft in tgt_blocks:
+        
+        src_blocks = dict((b.bid, b) for b in src_blocks if b.bid is not None)
+        for tgt_block in tgt_blocks:
             # check stored content hash
-
-            if bid is not None:
-                cnt = src_blocks[bid]
-
-            blocks.append((bid, bef, cnt, aft))
+            if tgt_block.bid is not None:
+                tgt_block.content = src_blocks[tgt_block.bid].content
+            
+            blocks.append(tgt_block)
     else:  # format non preserved blocks for the first and only time
-        for bid, bef, cnt, aft in src_blocks:
-            if bid is None:
-                cnt = cfg.render(cnt)
-
-            blocks.append((bid, bef, cnt, aft))
-
+        for block in src_blocks:
+            if block.bid is None:
+                block.content = cfg.render(block.content)
+            
+            blocks.append(block)
+    
     # regenerate preserved block content
-    # print "blocks", [bid for bid, bef, cnt, aft in blocks]
     preserved = []
     tgt = ""
-    for bid, bef, cnt, aft in blocks:
-        if bid is None:
-            tgt += cnt
+    for block in blocks:
+        if block.bid is None:
+            tgt += block.content
         else:
             # format cnt
-            cnt = cfg.render(cnt)
-            preserved.append((bid, cnt))
+            cnt = cfg.render(block.content)
+            preserved.append((block.bid, cnt))
             # rewrite preserved tag if necessary
-            tgt += bef + "{" + "# pkglts, %s\n" % bid
+            tgt += block.before_header + "{" + "# pkglts, %s\n" % block.bid
             tgt += cnt
-            tgt += "\n" + aft + "#" + "}\n"
-
+            tgt += "\n" + block.before_footer + "#" + "}\n"
+    
     with open(tgt_pth, 'w') as f:
         f.write(tgt)
-
+    
     return preserved
