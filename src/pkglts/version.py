9c31c8162fb076151f45435efd41464b14620df6
jchopard <jerome.chopard@itk.fr>
2017-11-28 10:53:10 +0100

    bump version

--- src/pkglts/version.py
+++ src/pkglts/version.py
@@ -2,8 +2,13 @@
 #  -*- coding: utf-8 -*-
 
 major = 2
-minor = 0
-post = 2
+"""(int) Version major component."""
+
+minor = 1
+"""(int) Version minor component."""
+
+post = 0
+"""(int) Version post or bugfix component."""
 
 __version__ = ".".join([str(s) for s in (major, minor, post)])
 # #}
