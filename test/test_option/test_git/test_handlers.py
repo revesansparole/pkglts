bb2f97d21957bd96803832e6a7923d38a5aef6c4
jchopard <jerome.chopard@itk.fr>
2017-12-05 17:18:03 +0100

    rm test from github and put it in git option

--- test/test_option/test_git/test_handlers.py
+++ test/test_option/test_git/test_handlers.py
@@ -5,15 +5,15 @@ from pkglts.config_management import Config
 
 def test_contributors_with_failed_request():
     with mock.patch('subprocess.check_output', side_effect=KeyError):
-        cfg = Config(dict(github={'owner': "moi", 'project': "project"}))
+        cfg = Config(dict(git={}))
         cfg.load_extra()
-        assert "failed" in cfg._env.globals['github'].contributors[0]
+        assert "failed" in cfg._env.globals['git'].contributors[0]
 
 
 def test_contributors_without_failed_request():
     commits = b'commit Author: a <toto@titi>\n, commit: Author: b <tata@titi>\n, commit: Author: b <tata@titi>\n'
     with mock.patch('subprocess.check_output', return_value=commits):
-        cfg = Config(dict(github={'owner': "moi", 'project': "project"}))
+        cfg = Config(dict(git={}))
         cfg.load_extra()
-        contributors = cfg._env.globals['github'].contributors
+        contributors = cfg._env.globals['git'].contributors
         assert len(contributors) == 2
