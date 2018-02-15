21e13e24361b8eac3b134c697f9d6c316f16582f
jchopard <jerome.chopard@itk.fr>
2018-02-15 07:11:13 +0100

    changed import to also work in python2

--- src/pkglts/tool/history.py
+++ src/pkglts/tool/history.py
@@ -4,10 +4,11 @@ This tool will try to parse all release tags to create an history of package.
 from functools import cmp_to_key
 import logging
 import os
-try:
+try:  # python3
     from urllib.parse import quote_plus
-except ImportError:
-    from six.moves.urllib_parse import quote_plus
+except ImportError:  # python2
+    from urllib import quote_plus
+
 import requests
 import semver
 
