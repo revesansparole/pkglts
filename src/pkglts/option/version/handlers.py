b092a5533d30823469818060cf26bb5c4826024c
jchopard <jerome.chopard@itk.fr>
2018-02-25 20:46:34 +0100

    version is new style option

--- src/pkglts/option/version/handlers.py
+++
@@ -1,9 +0,0 @@
-# from pkglts.templating import same
-#
-# try:
-#     from .github_versioning import fetch_github_version
-# except ImportError:
-#     fetch_github_version = same
-#
-#
-# mapping = {'version.fetch_github': fetch_github_version}
