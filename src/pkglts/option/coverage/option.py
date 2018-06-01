3bf92a40d800cca21b4901cc63749b3c548684de
rbourget <romain.bourget@itk.fr>
2018-06-01 14:41:43 +0200

    add exclude_lines option in coverage option

--- src/pkglts/option/coverage/option.py
+++ src/pkglts/option/coverage/option.py
@@ -2,11 +2,22 @@ from os.path import dirname
 
 from pkglts.dependency import Dependency
 from pkglts.option_object import Option
+from pkglts.option.pysetup.option import requirements
 
 
 class OptionCoverage(Option):
     def root_dir(self):
         return dirname(__file__)
+
+    def update_parameters(self, cfg):
+        sec = dict(
+            exclude_lines=[
+                "raise AssertionError",
+                "raise NotImplemented",
+                "raise NotImplementedError"
+            ],
+        )
+        cfg['coverage'] = sec
 
     def require(self, purpose, cfg):
         if purpose == 'option':
@@ -22,3 +33,10 @@ class OptionCoverage(Option):
             return deps
 
         return []
+
+    def environment_extensions(self, cfg):
+        if 'exclude_lines' in cfg['coverage'].keys():
+            exclude_lines = cfg['coverage']['exclude_lines']
+        else:
+            exclude_lines = []
+        return {"exclude_lines": exclude_lines}
