7faf4b4a60cc67c50153589f316a01bae7dfd189
jchopard <jerome.chopard@itk.fr>
2018-06-10 00:43:16 +0200

    rm prints

--- test/test_templating.py
+++ test/test_templating.py
@@ -2,8 +2,8 @@ from pkglts.templating import parse_source
 
 
 def test_parse_finds_all_blocks():
-    txt = ("print 'toto'\n"
-           "print 'titi'\n"
+    txt = ("assert 'toto'\n"
+           "assert 'titi'\n"
            "# {# pkglts, bid\n"
            "this is content\n"
            "# #}\n"
@@ -14,8 +14,8 @@ def test_parse_finds_all_blocks():
     assert b1.before_header == ""
     assert b1.bid is None
     assert b1.after_header == ""
-    assert b1.content == ("print 'toto'\n"
-                          "print 'titi'\n")
+    assert b1.content == ("assert 'toto'\n"
+                          "assert 'titi'\n")
     assert b1.before_footer == ""
     assert b1.after_footer == ""
 
@@ -35,8 +35,8 @@ def test_parse_finds_all_blocks():
 
 
 def test_parse_finds_bef_after_header():
-    txt = ("print 'toto'\n"
-           "print 'titi'\n"
+    txt = ("assert 'toto'\n"
+           "assert 'titi'\n"
            "# bef header{# pkglts, bid after header\n"
            "this is content\n"
            "# #}\n"
@@ -53,8 +53,8 @@ def test_parse_finds_bef_after_header():
 
 
 def test_parse_finds_bef_after_footer():
-    txt = ("print 'toto'\n"
-           "print 'titi'\n"
+    txt = ("assert 'toto'\n"
+           "assert 'titi'\n"
            "# {# pkglts, bid\n"
            "this is content\n"
            "# bef footer#} after footer\n"
@@ -75,8 +75,6 @@ def test_parse_finds_single_pkglts_block():
            "this is content\n"
            "# bef footer#} after footer\n")
     blocks = parse_source(txt)
-    for block in blocks:
-        print("bl", block)
     assert len(blocks) == 1
     block, = blocks
     assert block.before_header == "# "
