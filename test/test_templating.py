0774a70642b145956bc26c881529ffef9262a2f4
jchopard <jerome.chopard@itk.fr>
2018-06-10 00:13:01 +0200

    template handles single block, tests of parse are back

--- test/test_templating.py
+++ test/test_templating.py
@@ -1,6 +1,91 @@
-# from pkglts.templating import (get_comment_marker, parse,
-#                                reconstruct_txt_div, replace,
-#                                swap_divs)
+from pkglts.templating import parse_source
+
+
+def test_parse_finds_all_blocks():
+    txt = ("print 'toto'\n"
+           "print 'titi'\n"
+           "# {# pkglts, bid\n"
+           "this is content\n"
+           "# #}\n"
+           "after block")
+    blocks = parse_source(txt)
+    assert len(blocks) == 3
+    b1, b2, b3 = blocks
+    assert b1.before_header == ""
+    assert b1.bid is None
+    assert b1.after_header == ""
+    assert b1.content == ("print 'toto'\n"
+                          "print 'titi'\n")
+    assert b1.before_footer == ""
+    assert b1.after_footer == ""
+
+    assert b2.before_header == "# "
+    assert b2.bid == "bid"
+    assert b2.after_header == ""
+    assert b2.content == "this is content\n"
+    assert b2.before_footer == "# "
+    assert b2.after_footer == ""
+
+    assert b3.before_header == ""
+    assert b3.bid is None
+    assert b3.after_header == ""
+    assert b3.content == "after block"
+    assert b3.before_footer == ""
+    assert b3.after_footer == ""
+
+
+def test_parse_finds_bef_after_header():
+    txt = ("print 'toto'\n"
+           "print 'titi'\n"
+           "# bef header{# pkglts, bid after header\n"
+           "this is content\n"
+           "# #}\n"
+           "after block")
+    blocks = parse_source(txt)
+    assert len(blocks) == 3
+    _, block, _ = blocks
+    assert block.before_header == "# bef header"
+    assert block.bid == "bid"
+    assert block.after_header == " after header"
+    assert block.content == "this is content\n"
+    assert block.before_footer == "# "
+    assert block.after_footer == ""
+
+
+def test_parse_finds_bef_after_footer():
+    txt = ("print 'toto'\n"
+           "print 'titi'\n"
+           "# {# pkglts, bid\n"
+           "this is content\n"
+           "# bef footer#} after footer\n"
+           "after block")
+    blocks = parse_source(txt)
+    assert len(blocks) == 3
+    _, block, _ = blocks
+    assert block.before_header == "# "
+    assert block.bid == "bid"
+    assert block.after_header == ""
+    assert block.content == "this is content\n"
+    assert block.before_footer == "# bef footer"
+    assert block.after_footer == " after footer"
+
+
+def test_parse_finds_single_pkglts_block():
+    txt = ("# {# pkglts, bid\n"
+           "this is content\n"
+           "# bef footer#} after footer\n")
+    blocks = parse_source(txt)
+    for block in blocks:
+        print("bl", block)
+    assert len(blocks) == 1
+    block, = blocks
+    assert block.before_header == "# "
+    assert block.bid == "bid"
+    assert block.after_header == ""
+    assert block.content == "this is content\n"
+    assert block.before_footer == "# bef footer"
+    assert block.after_footer == " after footer"
+
 #
 #
 # def upper(txt, env):
