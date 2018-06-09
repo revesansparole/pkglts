d755e29b0c81155301c4138f6792ef86a5886b04
jchopard <jerome.chopard@itk.fr>
2018-06-10 01:01:49 +0200

    bump post, pmg rg

--- setup.py
+++ setup.py
@@ -34,7 +34,7 @@ pkg_data['pkglts'] = data_files
 
 setup_kwds = dict(
     name='pkglts',
-    version="3.5.1",
+    version="3.5.2",
     description=short_descr,
     long_description=readme + '\n\n' + history,
     author="revesansparole",
