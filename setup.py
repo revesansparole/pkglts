65eab070de2ec6e77840af8b34a1ea4008493832
jchopard <jerome.chopard@itk.fr>
2018-11-07 11:45:37 +0100

    rm py37 still problematic on travis

--- setup.py
+++ setup.py
@@ -78,7 +78,6 @@ setup_kwds = dict(
         "Programming Language :: Python :: 2.7",
         "Programming Language :: Python :: 3",
         "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
     ],
     )
 # #}
