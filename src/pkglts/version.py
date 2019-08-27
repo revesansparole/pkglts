aa7cd620a9330e9e149dd528d6fe3dba3b096421
jchopard <jerome.chopard@itk.fr>
2019-08-27 16:51:19 +0200

    bump major, pmg rg

--- src/pkglts/version.py
+++ src/pkglts/version.py
@@ -4,13 +4,13 @@ Maintain version for this package.
 # {# pkglts, version
 #  -*- coding: utf-8 -*-
 
-MAJOR = 4
+MAJOR = 5
 """(int) Version major component."""
 
-MINOR = 6
+MINOR = 0
 """(int) Version minor component."""
 
-POST = 1
+POST = 0
 """(int) Version post or bugfix component."""
 
 __version__ = ".".join([str(s) for s in (MAJOR, MINOR, POST)])
