2435c54f178b805956d9c9f9f94b22212479bdb1
jchopard <jerome.chopard@itk.fr>
2019-10-29 22:29:22 +0100

    improve coverage

--- test/test_option/test_reqs/test_handlers.py
+++ test/test_option/test_reqs/test_handlers.py
@@ -7,3 +7,6 @@ def test_requirements():
     cfg.load_extra()
     assert len(cfg._env.globals['reqs'].requirements('install')) == 0
     assert len(cfg._env.globals['reqs'].requirements('test')) == 2
+    assert len(cfg._env.globals['reqs'].intents()) >= 4
+    assert len(cfg._env.globals['reqs'].conda_reqs('install')) >= 0
+    assert len(cfg._env.globals['reqs'].pip_reqs('install')) >= 0
