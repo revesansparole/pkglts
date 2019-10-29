b5bae573bfaffdf0658da697bb7504e8ceb657fc
jchopard <jerome.chopard@itk.fr>
2019-10-29 22:41:12 +0100

    improve coverage

--- test/test_option/test_reqs/test_find_requirements.py
+++ test/test_option/test_reqs/test_find_requirements.py
@@ -1,8 +1,9 @@
 from pathlib import Path
 
 import pytest
-from pkglts.option.reqs.find_requirements import find_reqs
+from pkglts.option.reqs.find_requirements import find_reqs, action_find_reqs
 from pkglts.small_tools import ensure_created, rmdir
+from pkglts.config_management import Config
 
 
 @pytest.fixture()
@@ -36,3 +37,29 @@ def test_find_reqs_parses_files(tmp_dir):
 
     for name in ('os', 'sys', 'pytest', 'numpy', 'pandas', 'toto', 'doom'):
         assert name in reqs
+
+
+def test_myoutput(tmp_dir, capsys):
+    pth = tmp_dir / "toto.py"
+    with open(pth, 'w') as fhw:
+        fhw.write("import os\n"
+                  "from sys import argv\n"
+                  "\n"
+                  "import pytest\n"
+                  "import numpy as np\n"
+                  "from pandas import DataFrame\n"
+                  "from toto import failure\n"
+                  "\n"
+                  "a = 1\n"
+                  "\n"
+                  "from doom import pestilence\n"
+                  "")
+
+    cfg = Config(dict(base={'pkgname': "walou",
+                            'namespace': None,
+                            'owner': 'moi',
+                            'url': None}))
+    action_find_reqs(cfg)
+
+    out, err = capsys.readouterr()
+    assert "standard" in out
