e020252dade6987690e82af6ec84da1d964e868f
jchopard <jerome.chopard@itk.fr>
2020-10-22 15:40:00 +0200

    bump minor, pmg rg

--- setup.py
+++ setup.py
@@ -24,7 +24,7 @@ pkg_data = {'pkglts': data_files}
 
 setup_kwds = dict(
     name='pkglts',
-    version="5.1.10",
+    version="5.2.0",
     description=short_descr,
     long_description=readme + '\n\n' + history,
     author="revesansparole",
