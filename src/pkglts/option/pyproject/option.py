22f32c0b0b5775274fc29e56f1a8b81459458827
revesansparole <revesansparole@gmail.com>
2023-12-06 18:13:05 +0100

    standalone option pyproject no dep on pysetup

--- src/pkglts/option/pyproject/option.py
+++ src/pkglts/option/pyproject/option.py
@@ -17,15 +17,77 @@ class OptionPyproject(Option):
     def update_parameters(self, cfg):
         LOGGER.info("update parameters %s", self._name)
         sec = dict(
+            intended_versions=["3.10"],
         )
         cfg[self._name] = sec
 
     def check(self, cfg):
         invalids = []
+        intended_versions = cfg[self._name]['intended_versions']
+
+        if not intended_versions:
+            invalids.append("pyproject.intended_versions")
+
         return invalids
 
     def require_option(self, cfg):
-        return ['pysetup']
+        return ['src', 'doc', 'license', 'version', 'reqs']
 
     def environment_extensions(self, cfg):
-        return {}
+        py_vers = sorted([int(v) for v in ver.split(".")] for ver in cfg[self._name]['intended_versions'])
+
+        universal = len(set(ver.split(".")[0] for ver in cfg[self._name]['intended_versions'])) > 1
+
+        return {
+            "pkg_url": pkg_url(cfg),
+            "py_max_ver": ".".join(str(v) for v in py_vers[-1]),
+            "py_min_ver": ".".join(str(v) for v in py_vers[0]),
+            "universal": universal
+        }
+
+
+def pkg_url(cfg):
+    """Extract a valid url from all config sections.
+
+    Args:
+        cfg (Config):  current package configuration
+
+    Returns:
+        (str): a valid url for the package
+    """
+    try:
+        url = cfg['base']['url']
+        if url is not None:
+            return url
+    except KeyError:
+        pass
+
+    try:
+        url = cfg['github']['url']
+        if url is not None:
+            return url
+    except KeyError:
+        pass
+
+    try:
+        url = cfg['gitlab']['url']
+        if url is not None:
+            return url
+    except KeyError:
+        pass
+
+    # try:
+    #     url = cfg['pypi']['url']
+    #     if url is not None:
+    #         return url
+    # except KeyError:
+    #     pass
+
+    # try:
+    #     url = cfg['readthedocs']['url']
+    #     if url is not None:
+    #         return url
+    # except KeyError:
+    #     pass
+
+    return ""
--- src/pkglts/option/pyproject/resource/setup.py.tpl
+++
@@ -1,8 +0,0 @@
-# {# pkglts, pyproject.kwds, replace pysetup.kwds
-from setuptools import setup
-# #}
-
-# {# pkglts, pyproject.call, replace pysetup.call
-if __name__ == "__main__":
-    setup()
-# #}
