152d5028aad1cf08a6615fb655678b596242f08d
revesansparole <revesansparole@gmail.com>
2024-05-17 10:58:05 +0200

    bump minor, pmg rg

--- doc/conf.py
+++ doc/conf.py
@@ -79,9 +79,9 @@ copyright = u"2015, pkglts"
 #
 
 # The short X.Y version.
-version = "7.2.0"
+version = "7.3.0"
 # The full version, including alpha/beta/rc tags.
-release = "7.2.0"
+release = "7.3.0"
 
 
 exclude_patterns = ['build', 'dist']
