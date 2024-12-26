a70545a9b6e2426590cb28650af1213410a33e20
revesansparole <revesansparole@gmail.com>
2024-12-26 15:13:38 +0100

    conda alias

---
+++ src/pkglts/option/conda_alias/option.py
@@ -0,0 +1,28 @@
+import logging
+from pathlib import Path
+
+from pkglts.option_object import Option
+from pkglts.version import __version__
+from pkglts.dependency import normalize
+from pkglts.local import pkg_full_name
+
+LOGGER = logging.getLogger(__name__)
+
+
+class OptionCondaAlias(Option):
+    def version(self):
+        return __version__
+
+    def root_dir(self):
+        return Path(__file__).parent
+
+    def check(self, cfg):
+        invalids = []
+
+        return invalids
+
+    def require_option(self, cfg):
+        return ['conda']
+
+    def environment_extensions(self, cfg):
+        return {"norm_name": normalize(pkg_full_name(cfg))}
