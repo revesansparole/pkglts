541806fe7dcf1e09bb93e71177e9dbd02eeabda3
revesansparole <revesansparole@gmail.com>
2026-01-28 14:11:55 +0100

    bump post, pmg rg

--- doc/conf.py
+++ doc/conf.py
@@ -80,9 +80,9 @@ copyright = "2015, pkglts"
 #
 
 # The short X.Y version.
-version = "7.11.0"
+version = "7.11.1"
 # The full version, including alpha/beta/rc tags.
-release = "7.11.0"
+release = "7.11.1"
 
 
 exclude_patterns = ['build', 'dist']
